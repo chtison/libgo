f83bf81d13b25863508f2a0b73cb4db940c22dcc
chtison <ch4rles.contact@gmail.com>
2017-01-28 10:22:22 +0100

    .

---
+++ tmpl/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"text/template"
+
+	"github.com/chtison/libgo/fmt"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		usage()
+	}
+	tmpl, err := template.ParseFiles(os.Args[1])
+	if err != nil {
+		fmt.Fprintfln(os.Stderr, `error: %s: %s`, os.Args[1], err)
+		os.Exit(1)
+	}
+	data, err := ioutil.ReadFile(os.Args[2])
+	if err != nil {
+		fmt.Fprintfln(os.Stderr, `error: %s: %s`, os.Args[2], err)
+		os.Exit(1)
+	}
+	var m map[string]interface{}
+	if err = json.Unmarshal(data, &m); err != nil {
+		fmt.Fprintfln(os.Stderr, `error: %s: %s`, os.Args[2], err)
+		os.Exit(1)
+	}
+	outfile, err := os.Create(os.Args[3])
+	if err != nil {
+		fmt.Fprintfln(os.Stderr, `error: %s: %s`, os.Args[3], err)
+		os.Exit(1)
+	}
+	defer outfile.Close()
+	if tmpl.Execute(outfile, m); err != nil {
+		fmt.Fprintfln(os.Stderr, `error: processing template failed: %s`, err)
+		outfile.Close()
+		os.Remove(os.Args[3])
+		os.Exit(1)
+	}
+}
+
+func usage() {
+	fmt.Printfln(`usage: %s TEMPLATE JSONFILE RESULT
+TEMPLATE: the template processed
+DATAFILE: a JSON formatted file serving as environment for template
+RESULT  : result of the processed template`, os.Args[0])
+	os.Exit(1)
+}
