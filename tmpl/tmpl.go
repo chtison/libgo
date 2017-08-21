5a8935fb2779c789efa1363dbfba621335e53eac
chtison <ch4rles.contact@gmail.com>
2017-08-21 17:27:04 +0200

    Move commands to root cmd directory

--- tmpl/tmpl.go
+++ tmpl/tmpl.go
@@ -1,55 +1,19 @@
-package main
+package tmpl
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"strings"
 	"text/template"
 	"time"
-
-	"github.com/chtison/libgo/fmt"
-	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	if len(os.Args) < 4 {
-		fmt.Printfln(`usage: %s OUTPUT_FILE DATA_YAML TEMPLATE [...]`, os.Args[0])
-		os.Exit(1)
-	}
-	funcMap := template.FuncMap{
+// Funcs ...
+func Funcs() template.FuncMap {
+	return template.FuncMap{
 		"dictStringToInterface":    dictStringToInterface,
 		"dictInterfaceToInterface": dictInterfaceToInterface,
 		"time":    NewTime,
 		"strings": NewString,
 	}
-	tmpl, err := template.New(os.Args[3]).Funcs(funcMap).ParseFiles(os.Args[3:]...)
-	if err != nil {
-		fmt.Fprintfln(os.Stderr, `error: %v: %s`, os.Args[3:], err)
-		os.Exit(1)
-	}
-	data, err := ioutil.ReadFile(os.Args[2])
-	if err != nil {
-		fmt.Fprintfln(os.Stderr, `error: %s: %s`, os.Args[2], err)
-		os.Exit(1)
-	}
-	var m map[string]interface{}
-	if err = yaml.Unmarshal(data, &m); err != nil {
-		fmt.Fprintfln(os.Stderr, `error: %s: %s`, os.Args[2], err)
-		os.Exit(1)
-	}
-	outfile, err := os.Create(os.Args[1])
-	if err != nil {
-		fmt.Fprintfln(os.Stderr, `error: %s: %s`, os.Args[1], err)
-		os.Exit(1)
-	}
-	if err := tmpl.ExecuteTemplate(outfile, filepath.Base(os.Args[3]), m); err != nil {
-		fmt.Fprintfln(os.Stderr, `error: processing template failed: %s`, err)
-		outfile.Close()
-		os.Remove(os.Args[3])
-		os.Exit(1)
-	}
-	outfile.Close()
 }
 
 func dictStringToInterface(d map[string]interface{}, values ...interface{}) map[string]interface{} {
